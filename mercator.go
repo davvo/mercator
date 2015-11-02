7f4b1730b767db934c161491cadaed58e51e3fe2
David Ershag <david.ershag@gmail.com>
2015-11-02 21:49:44 +0100

    Godoc fixes

--- mercator.go
+++ mercator.go
@@ -1,3 +1,5 @@
+// Package mercator contains conversion tools for the Global Mercator coordinate system
+// See http://www.maptiler.org/google-maps-coordinates-tile-bounds-projection/
 package mercator
 
 import "math"
@@ -13,18 +15,18 @@ func round(a float64) float64 {
 	return math.Floor(a + 0.5)
 }
 
-// Resolution (meters/pixel) for given zoom level (measured at Equator)
+// Resolution calculates the resolution (meters/pixel) for given zoom level (measured at Equator)
 func Resolution(zoom int) float64 {
 	return initialResolution / math.Pow(2, float64(zoom))
 }
 
-// Zoom level for given resolution (measured at Equator)
+// Zoom gives the zoom level for given resolution (measured at Equator)
 func Zoom(resolution float64) int {
 	zoom := round(math.Log(initialResolution/resolution) / math.Log(2))
 	return int(zoom)
 }
 
-// Converts given lat/lon in WGS84 Datum to XY in Spherical Mercator EPSG:900913
+// LatLonToMeters converts given lat/lon in WGS84 Datum to XY in Spherical Mercator EPSG:900913
 func LatLonToMeters(lat, lon float64) (x, y float64) {
 	x = lon * originShift / 180
 	y = math.Log(math.Tan((90+lat)*math.Pi/360)) / (math.Pi / 180)
@@ -32,7 +34,7 @@ func LatLonToMeters(lat, lon float64) (x, y float64) {
 	return x, y
 }
 
-// Converts XY point from Spherical Mercator EPSG:900913 to lat/lon in WGS84 Datum
+// MetersToLatLon converts XY point from Spherical Mercator EPSG:900913 to lat/lon in WGS84 Datum
 func MetersToLatLon(mx, my float64) (lat, lon float64) {
 	lon = (mx / originShift) * 180
 	lat = (my / originShift) * 180
@@ -40,7 +42,7 @@ func MetersToLatLon(mx, my float64) (lat, lon float64) {
 	return lat, lon
 }
 
-// Converts pixel coordinates in given zoom level of pyramid to EPSG:900913
+// PixelsToMeters converts pixel coordinates in given zoom level of pyramid to EPSG:900913
 func PixelsToMeters(px, py float64, zoom int) (x, y float64) {
 	res := Resolution(zoom)
 	x = px*res - originShift
@@ -48,7 +50,7 @@ func PixelsToMeters(px, py float64, zoom int) (x, y float64) {
 	return x, y
 }
 
-// Converts EPSG:900913 to pixel coordinates in given zoom level
+// MetersToPixels converts EPSG:900913 to pixel coordinates in given zoom level
 func MetersToPixels(x, y float64, zoom int) (px, py float64) {
 	res := Resolution(zoom)
 	px = (x + originShift) / res
@@ -56,32 +58,32 @@ func MetersToPixels(x, y float64, zoom int) (px, py float64) {
 	return px, py
 }
 
-// Converts given lat/lon in WGS84 Datum to pixel coordinates in given zoom level
+// LatLonToPixels converts given lat/lon in WGS84 Datum to pixel coordinates in given zoom level
 func LatLonToPixels(lat, lon float64, zoom int) (px, py float64) {
 	x, y := LatLonToMeters(lat, lon)
 	return MetersToPixels(x, y, zoom)
 }
 
-// Converts pixel coordinates in given zoom level to lat/lon in WGS84 Datum
+// PixelsToLatLon converts pixel coordinates in given zoom level to lat/lon in WGS84 Datum
 func PixelsToLatLon(px, py float64, zoom int) (lat, lon float64) {
 	x, y := PixelsToMeters(px, py, zoom)
 	return MetersToLatLon(x, y)
 }
 
-// Returns a tile covering region in given pixel coordinates
+// PixelsToTile returns a tile covering region in given pixel coordinates
 func PixelsToTile(px, py float64) (int, int) {
 	tileX := int(math.Floor(px / tileSize))
 	tileY := int(math.Floor(py / tileSize))
 	return tileX, tileY
 }
 
-// Returns tile for given mercator coordinates
+// MetersToTile returns tile for given mercator coordinates
 func MetersToTile(x, y float64, zoom int) (int, int) {
 	px, py := MetersToPixels(x, y, zoom)
 	return PixelsToTile(px, py)
 }
 
-// Returns tile for given lat/lon coordinates
+// LatLonToTile returns tile for given lat/lon coordinates
 func LatLonToTile(lat, lon float64, zoom int) (int, int) {
 	px, py := LatLonToPixels(lat, lon, zoom)
 	return PixelsToTile(px, py)
